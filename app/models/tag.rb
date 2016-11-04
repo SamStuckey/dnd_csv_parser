f2cbc905b80e8a69158de630a9dfb58b9ccb0c9d
SamStuckey <sam.n.stuckey@gmail.com>
2016-11-04 15:45:10 -0400

    Create tag creation validations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,11 @@
 class Tag < ActiveRecord::Base
+  validates :description, length: { minimum: 3 }, uniqueness: true
+  validate :letters_and_numbers_only_in_description
+
+  private
+  def letters_and_numbers_only_in_description
+    if description[/[a-zA-Z\d]*/] != description
+      errors.add(:description, 'is not a vaild description')
+    end
+  end
 end
